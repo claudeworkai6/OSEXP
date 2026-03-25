7a4d18428201ddb19373e94921728e2873afd449
claudeworkai6 <claudeworkai6@gmail.com>
2026-03-25 10:07:10 +0530

    Add Banker's algorithm to dining philosopher simulation
    Implemented Banker's algorithm for deadlock avoidance in dining philosophers problem.

--- dining_philosopher.c
+++ dining_philosopher.c
@@ -1,38 +1,31 @@
-#include <stdio.h>
-#include <pthread.h>
-#include <unistd.h>
-
-#define N 5
-pthread_mutex_t f[N];
-
-void* p(void* a){
-    int i=*(int*)a;
-
-    if(i%2==0){
-        pthread_mutex_lock(&f[i]);
-        pthread_mutex_lock(&f[(i+1)%N]);
-    } else {
-        pthread_mutex_lock(&f[(i+1)%N]);
-        pthread_mutex_lock(&f[i]);
-    }
-
-    printf("P%d eating\n",i);
-    sleep(1);
-
-    pthread_mutex_unlock(&f[i]);
-    pthread_mutex_unlock(&f[(i+1)%N]);
-}
+#include<stdio.h>
+#define PROCESSES 5
+#define RESOURCES 3
 
 int main(){
-    pthread_t t[N];
-    int i,id[N];
+ int allocation[PROCESSES][RESOURCES]={{0,1,0},{2,0,0},{3,0,2},{2,1,1},{0,0,2}};
+ int max[PROCESSES][RESOURCES]={{7,5,3},{3,2,2},{9,0,2},{2,2,2},{4,3,3}};
+ int available[RESOURCES]={3,3,2},need[PROCESSES][RESOURCES],finish[PROCESSES]={0},work[RESOURCES],safeSeq[PROCESSES];
+ int i,j,k,completed=0;
 
-    for(i=0;i<N;i++) pthread_mutex_init(&f[i],NULL);
+ for(i=0;i<PROCESSES;i++)
+ for(j=0;j<RESOURCES;j++)
+  need[i][j]=max[i][j]-allocation[i][j];
 
-    for(i=0;i<N;i++){
-        id[i]=i;
-        pthread_create(&t[i],NULL,p,&id[i]);
-    }
+ for(i=0;i<RESOURCES;i++) work[i]=available[i];
 
-    for(i=0;i<N;i++) pthread_join(t[i],NULL);
+ while(completed<PROCESSES){
+  int found=0;
+  for(i=0;i<PROCESSES;i++) if(!finish[i]){
+   for(j=0;j<RESOURCES && need[i][j]<=work[j];j++);
+   if(j==RESOURCES){
+    for(k=0;k<RESOURCES;k++) work[k]+=allocation[i][k];
+    safeSeq[completed++]=i; finish[i]=1; found=1;
+   }
+  }
+  if(!found){ printf("NOT SAFE"); return 0; }
+ }
+
+ printf("SAFE\n");
+ for(i=0;i<PROCESSES;i++) printf("P%d ",safeSeq[i]);
 }
