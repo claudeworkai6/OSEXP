751eb90ce95abf5868b3b0f9ae2d61844658834c
claudeworkai6 <claudeworkai6@gmail.com>
2026-03-25 09:12:30 +0530

    Refactor deadlock detection algorithm for clarity

--- deadlock_banker's algorithm.c
+++ deadlock_banker's algorithm.c
@@ -1,41 +1,35 @@
-#include <stdio.h>
+#include<stdio.h>
 #define P 5
 #define R 3
 
 int main(){
-    int alloc[P][R]={{0,1,0},{2,0,0},{3,0,2},{2,1,1},{0,0,2}};
-    int max[P][R]={{7,5,3},{3,2,2},{9,0,2},{2,2,2},{4,3,3}};
-    int avail[R]={3,3,2};
+ int alloc[P][R]={{0,1,0},{2,0,0},{3,0,2},{2,1,1},{0,0,2}};
+ int max[P][R]={{7,5,3},{3,2,2},{9,0,2},{2,2,2},{4,3,3}};
+ int avail[R]={3,3,2};
 
-    int need[P][R],finish[P]={0},safe[P],work[R],i,j,k,c=0;
+ int need[P][R],finish[P]={0},work[R],i,j,k,c=0;
 
-    for(i=0;i<P;i++)
+ for(i=0;i<P;i++)
+ for(j=0;j<R;j++)
+  need[i][j]=max[i][j]-alloc[i][j];
+
+ for(i=0;i<R;i++) work[i]=avail[i];
+
+ while(c<P){
+  int f=0;
+  for(i=0;i<P;i++){
+   if(!finish[i]){
     for(j=0;j<R;j++)
-        need[i][j]=max[i][j]-alloc[i][j];
+     if(need[i][j]>work[j]) break;
 
-    for(i=0;i<R;i++) work[i]=avail[i];
-
-    while(c<P){
-        int found=0;
-        for(i=0;i<P;i++){
-            if(!finish[i]){
-                for(j=0;j<R;j++)
-                    if(need[i][j]>work[j]) break;
-
-                if(j==R){
-                    for(k=0;k<R;k++) work[k]+=alloc[i][k];
-                    safe[c++]=i;
-                    finish[i]=1;
-                    found=1;
-                }
-            }
-        }
-        if(!found){
-            printf("UNSAFE");
-            return 0;
-        }
+    if(j==R){
+     for(k=0;k<R;k++) work[k]+=alloc[i][k];
+     finish[i]=1; c++; f=1;
     }
+   }
+  }
+  if(!f){ printf("UNSAFE"); return 0; }
+ }
 
-    printf("SAFE\nSequence: ");
-    for(i=0;i<P;i++) printf("P%d ",safe[i]);
+ printf("SAFE");
 }
